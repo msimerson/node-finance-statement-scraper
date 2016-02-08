c72ce0cdef12830a6d0dfa9b01c7ee7693ecd430
Matt Simerson <matt@tnpi.net>
2016-02-07 21:12:41 -0800

    add balance sheet & cash flow

---
+++ test/lib/yahoo.js
@@ -0,0 +1,119 @@
+'use strict';
+
+var assert  = require('assert');
+var fs      = require('fs');
+var util    = require('util');
+
+var yahoo   = require('../../lib/yahoo');
+
+describe('yahoo', function() {
+
+  describe('fetchIncomeStatement', function () {
+    it('gets an Income Statement', function (done) {
+      yahoo.fetchIncomeStatement({symbol: 'AAPL'}, function (err, html) {
+        assert.ifError(err);
+        // console.log(util.inspect(html, {depth: null}));
+        assert.ok(html);
+        done();
+        // fs.writeFile('test/fixtures/AAPL-Yahoo-IS.html', html, function (err) {
+        //   if (err) {
+        //     console.error(err);
+        //   }
+        //   done();
+        // });
+      });
+    });
+  });
+
+  describe('parseIncomeStatement', function () {
+    it('parses a Yahoo Income Statement from raw HTML', function (done) {
+      fs.readFile('test/fixtures/AAPL-Yahoo-IS.html', function (err, data) {
+        assert.ifError(err);
+        assert.ok(data);
+        var contents = data.toString();
+        // console.log(util.inspect(contents, {depth: null}));
+        yahoo.parseIncomeStatement(contents, function (err, parsed) {
+          assert.ifError(err);
+          assert.ok(parsed);
+          console.log(util.inspect(parsed, {depth: null}));
+          // .$('.yfnc_tabledata1')
+          done();
+        });
+      });
+    });
+  });
+
+  describe.skip('getIncomeStatement', function () {
+    it('fetches and parses a Yahoo Income Statement', function (done) {
+      yahoo.getIncomeStatement({symbol: 'AAPL'}, function (err, report) {
+        assert.ifError(err);
+        assert.ok(report['Total Revenue'][0]);
+        done();
+      });
+    });
+  });
+
+  describe('fetchBalanceSheet', function () {
+    it('gets a Balance Sheet', function (done) {
+      yahoo.fetchBalanceSheet({symbol: 'AAPL'}, function (err, html) {
+        assert.ifError(err);
+        assert.ok(html);
+        done();
+        // fs.writeFile('test/fixtures/AAPL-Yahoo-BS.html', html, function (err) {
+        //   if (err) {
+        //     console.error(err);
+        //   }
+        //   done();
+        // });
+      });
+    });
+  });
+
+  describe('parseBalanceSheet', function () {
+    it('parses a Yahoo Balance Sheet from raw HTML', function (done) {
+      fs.readFile('test/fixtures/AAPL-Yahoo-BS.html', function (err, data) {
+        assert.ifError(err);
+        assert.ok(data);
+        var contents = data.toString();
+        yahoo.parseBalanceSheet(contents, function (err, report) {
+          assert.ifError(err);
+          assert.ok(report['Total Assets'][0]);
+          console.log(util.inspect(report, {depth: null}));
+          done();
+        });
+      });
+    });
+  });
+
+  describe('fetchCashFlow', function () {
+    it('gets a Cash Flow statement', function (done) {
+      yahoo.fetchCashFlow({symbol: 'AAPL'}, function (err, html) {
+        assert.ifError(err);
+        assert.ok(html);
+        done();
+        // fs.writeFile('test/fixtures/AAPL-Yahoo-CF.html', html, function (err) {
+        //   if (err) {
+        //     console.error(err);
+        //   }
+        //   done();
+        // });
+      });
+    });
+  });
+
+  describe('parseCashFlow', function () {
+    it('parses a Yahoo Cash Flow from raw HTML', function (done) {
+      fs.readFile('test/fixtures/AAPL-Yahoo-CF.html', function (err, data) {
+        assert.ifError(err);
+        assert.ok(data);
+        var contents = data.toString();
+        yahoo.parseCashFlow(contents, function (err, report) {
+          assert.ifError(err);
+          assert.ok(report['Depreciation'][0]);
+          console.log(util.inspect(report, {depth: null}));
+          done();
+        });
+      });
+    });
+  });
+});
